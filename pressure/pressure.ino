b2ce790721c3e85614676bf7a92c9a1326003695
Z0R7N <46117076+Z0R7N@users.noreply.github.com>
2022-12-21 14:54:10 +0300

    Update pressure.ino

--- pressure/pressure.ino
+++ pressure/pressure.ino
@@ -1,9 +1,10 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  pinMode(A0, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int x = analogRead(A0);
+  Serial.println(x);
+  delay(1000);
 }
